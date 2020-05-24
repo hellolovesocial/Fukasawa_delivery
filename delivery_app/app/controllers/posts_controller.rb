910ad4156dc29cec8b7774b91c290994a72332e3
hellolovesocial <taylor.minako375@gmail.com>
2020-05-24 16:29:03 +0900

    delete things around DB

--- delivery_app/app/controllers/posts_controller.rb
+++ delivery_app/app/controllers/posts_controller.rb
@@ -1,9 +1,9 @@
 class PostsController < ApplicationController
   def index
-    @posts = Post.all
-    # @posts = [
-    #   "東京夜市 TOKYO YOICHI",
-    #   "mano cafe"
-    #   ]
+    # @posts = Post.all
+    @posts = [
+      "東京夜市 TOKYO YOICHI",
+      "mano cafe"
+      ]
   end
 end
